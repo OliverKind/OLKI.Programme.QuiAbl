c1eb3a2f78fed294cd1313b5bc4026c670cac5f8
Oliver Kind <github@oliver-kind.de>
2022-02-07 20:15:57 +0100

    Update to version 2.4.2.25

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.1.24")]
-[assembly: AssemblyFileVersion("2.4.1.24")]
+[assembly: AssemblyVersion("2.4.2.25")]
+[assembly: AssemblyFileVersion("2.4.2.25")]
