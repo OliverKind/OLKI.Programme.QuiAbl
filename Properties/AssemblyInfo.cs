578cd5050416fb120fe002bc26be89f479656d71
Oliver Kind <github@oliver-kind.de>
2024-03-01 16:15:36 +0100

    Update to version 3.2.0.36

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.1.35")]
-[assembly: AssemblyFileVersion("3.1.1.35")]
+[assembly: AssemblyVersion("3.2.0.36")]
+[assembly: AssemblyFileVersion("3.2.0.36")]
